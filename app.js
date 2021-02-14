e2950686c63e9385f4ab43d1a886fa7f8ce1f288
Sajjad Sadiq <sajjadsadiq2020@gmail.com>
2021-02-14 20:48:45 +0600

    Basic Output

---
+++ app.js
@@ -0,0 +1,55 @@
+//01
+// Start Learn Javascript! 
+// JavaScript is easy to learn, understand than text explanations.
+
+// Programming Learning Speed || Practice. Practice. Practice. Code. Code. Code ! -  take a break!
+
+const one = 5;
+const two = 10;
+//console.log(one+two);
+/*
+document.getElementById('js').innerHTML = 'Bangaldesh! Dhaka'
+document.getElementById('js').style.fontSize = '30px'
+document.getElementById('js').style.fontFamily = 'roboto'
+document.getElementById('js').style.display = 'none' // Display Hiden
+document.getElementById('js').style.display = 'block'
+*/
+
+
+//02
+// JavaScript Where To || The <script> Tag
+// Functions and Events || <head> or <body>
+// function LearnJs(){
+//     document.getElementById('js').innerHTML='Enjoy JavaScript!';
+    
+// }
+
+
+// 03
+// JavaScript Output || Display Possibilities
+// HTML Element - .innerHTML  || Output - document.write()|| Alert Box - window.alert() || Browser Console - console.log().
+
+
+// Using innerHTML - document.getElementById(id)
+//document.getElementById('math').innerHTML = 5 + 4;
+
+//Using document.write() 
+//document.write("Enjoy Javascript Learn")
+
+// Using window.alert()
+//window.alert(3+6)
+
+// Using console.log()
+// console.log(9+3);
+// console.log('Browser Console');
+
+// JavaScript Print
+//document.getElementById('btn').onclick = "window.print()"
+
+
+
+// 04
+// JavaScript Statements
+
+
+
