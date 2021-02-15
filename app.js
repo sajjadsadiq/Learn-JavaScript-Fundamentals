37e788c4ecbce9588f47c6e990be1412dabfa32e
Sajjad Sadiq <sajjadsadiq2020@gmail.com>
2021-02-15 21:03:53 +0600

    JS Statements

--- app.js
+++ app.js
@@ -49,7 +49,19 @@ document.getElementById('js').style.display = 'block'
 
 
 // 04
-// JavaScript Statements
+// JavaScript Statements and Programs
+// A computer program is a list of "instructions" to be "executed" by a computer.
+// programming instructions are called statements.
+// A JavaScript program is a list of programming statements.
+// One Line Statements and Multiplay line Statements.
+var a, b, c;     // Declare 3 variables
+a = 5;           // Assign the value 5 to a
+b = 6;           // Assign the value 6 to b
+c = a + b;       // Assign the sum of a and b to c
+
+
+
+// JavaScript White Space -JavaScript ignores multiple spaces. 
 
 
 
